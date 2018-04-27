21b9c5b4bd501b5e4d74a32c7c6efb02c5858550
suddenlyGiovanni <ravalico.giovanni@gmail.com>
2018-04-27 22:20:54 +0200

    UPDATE

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 
 import * as Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter() });
 
 import App from './App';
 
