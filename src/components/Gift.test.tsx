4deca9b7c22363ba4095ec13870262ea7873c1d4
suddenlyGiovanni <ravalico.giovanni@gmail.com>
2018-04-28 20:52:57 +0200

    Gift

--- src/components/Gift.test.tsx
+++ src/components/Gift.test.tsx
@@ -1,9 +1,12 @@
+import * as Enzyme from 'enzyme';
 import * as React from 'react';
-import { shallow } from 'enzyme';
 import Gift from './Gift';
 
 describe('Gift', () => {
-  const gift = shallow(<Gift />);
+  const mockRemove = jest.fn();
+  const id = 1;
+  const props = { gift: { id }, removeGift: mockRemove };
+  const gift = Enzyme.shallow(<Gift {...props} />);
 
   it('renders properly', () => {
     expect(gift).toMatchSnapshot();
@@ -11,5 +14,39 @@ describe('Gift', () => {
 
   it('initializes a person and a present in `state`', () => {
     expect(gift.state()).toEqual({ person: '', present: '' });
+  });
+
+  describe('when typing into the person input', () => {
+    const person = 'Uncle';
+
+    beforeEach(() => {
+      gift.find('.input-person').simulate('change', { currentTarget: { value: person } });
+    });
+
+    it('updates the person in `state`', () => {
+      expect(gift.state().person).toEqual(person);
+    });
+  });
+
+  describe('when typing intp the present input', () => {
+    const present = 'Golf Clubs';
+
+    beforeEach(() => {
+      gift.find('.input-present').simulate('change', { currentTarget: { value: present } });
+    });
+
+    it('updates the present in `state`', () => {
+      expect(gift.state().present).toEqual(present);
+    });
+  });
+
+  describe('when clicking the `Remove Gift` button', () => {
+    beforeEach(() => {
+      gift.find('.btn-remove').simulate('click');
+    });
+
+    it('calls the removeGift callback', () => {
+      expect(mockRemove).toHaveBeenCalledWith(id);
+    });
   });
 });
--- src/components/Gift.tsx
+++ src/components/Gift.tsx
@@ -1,19 +1,53 @@
 import * as React from 'react';
+import { Button, ControlLabel, Form, FormControl, FormGroup } from 'react-bootstrap';
 
+interface Props {
+  gift: { id: number };
+  removeGift: (id: number) => void;
+}
 interface State {
   person: string;
   present: string;
 }
 
-export default class Gift extends React.Component<any, State> {
+export default class Gift extends React.Component<Props, State> {
   constructor(props: any) {
     super(props);
-    this.state = {
-      person: '',
-      present: ''
-    };
+    this.state = { person: '', present: '' };
   }
+
+  public handleInputPerson = (e: React.SyntheticEvent<any>): void => {
+    this.setState({ person: e.currentTarget.value });
+  };
+
+  public handleInputPresent = (e: React.SyntheticEvent<any>): void => {
+    this.setState({ present: e.currentTarget.value });
+  };
+
+  public handleButtonRemove = (): void => this.props.removeGift(this.props.gift.id);
+
   public render() {
-    return <div />;
+    return (
+      <div className="gift">
+        <Form>
+          <FormGroup>
+            <ControlLabel>Person</ControlLabel>
+            <FormControl className="input-person" name="person" onChange={this.handleInputPerson} />
+          </FormGroup>
+
+          <FormGroup>
+            <ControlLabel>Present</ControlLabel>
+            <FormControl
+              className="input-present"
+              name="present"
+              onChange={this.handleInputPresent}
+            />
+          </FormGroup>
+        </Form>
+        <Button className="btn-remove" onClick={this.handleButtonRemove}>
+          Remove Gift
+        </Button>
+      </div>
+    );
   }
 }
